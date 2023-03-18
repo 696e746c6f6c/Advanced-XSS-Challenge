4ed3d39ba5ad3b4a24200139f9922e1cd97ce4b1
int <96709073+int-wsl@users.noreply.github.com>
2023-03-19 00:49:18 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,31 @@
+let params = new URLSearchParams(window.location.search);
+let name = params.get('query');
+let input = document.getElementById('query');
+let form = document.querySelector('form');
+let hiDiv = document.getElementById('hi');
+
+if (name) {
+    input.value = name;
+}
+
+function checkInput(query) {
+    let regex = /^[\w\s.,!&'()\#$\-]{0,25}$/; // Hint: letters and Spaces are only allowed
+    return regex.test(query);
+}
+
+function formInput(event) {
+    event.preventDefault();
+    let query = input.value;
+
+    if (checkInput(query)) {
+        let hi = document.createElement('a');
+        hi.innerHTML = `Hello, ${query}!`;
+        hiDiv.appendChild(hi);
+    } else {
+        let invalidation = document.createElement('p');
+        invalidation.innerHTML = 'Nice try!';
+        hiDiv.appendChild(invalidation);
+    }
+}
+
+form.addEventListener('submit', formInput);
